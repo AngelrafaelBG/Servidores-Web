ffc723faeedc69667e23b946cc3a8300ba286115
AngelrafaelBG <angelrafaelmaldonaddo@gmail.com>
2026-03-16 14:22:30 -0600

    Configurar base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Servidores-Web/', // ¡Asegúrate de poner los slashes / /!
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/Servidores-Web/', 
+})
